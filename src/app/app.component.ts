f94c1b43e01431c3a80d7eb2e7786d4657fc6d7d
Vepathakpawan <pawanved007@gmail.com>
2021-04-20 07:49:31 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http'
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'APIcalling';
+apiUrl = 'http://jsonplaceholder.typicode.com/users';
+apiData: any;
+constructor(private http: HttpClient){
+    
+}
+ngOnInit(){
+this.http.get(this.apiUrl).subscribe((data)=>{
+console.warn(data)
+this.apiData=data;
+})
+}
 }
